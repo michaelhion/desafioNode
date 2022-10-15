b0c3104acd931d27b46fa51f9a3f7909cd9c3479
Michael Hion <michael.hion1|@gmail.com>
2022-10-15 16:49:41 -0300

    jwt concluido

--- src/client-controller/client-controller.module.ts
+++ src/client-controller/client-controller.module.ts
@@ -3,13 +3,19 @@ import { ClientControllerService } from './client-controller.service';
 import { ClientControllerController } from './client-controller.controller';
 import { DatabaseModule } from 'src/database.module';
 import { clientProviders } from './entities/client.providers';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Module({
   imports:[DatabaseModule],
   controllers: [ClientControllerController],
   providers: [
     ...clientProviders,
-    ClientControllerService
+    ClientControllerService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
   exports:[
     ClientControllerService
